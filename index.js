dabbe1059505f99e9ee6f00ee5cbefb1edfa3303
Elerin Noorkoiv <elerin.noorkoiv@voco.ee>
2023-10-17 13:30:29 +0300

    master

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+
+// use ejs files to prepare templates for views
+const path = require('path')
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (req ,res) => {
+	res.send('index')
+})
+
+app.listen(3001, () => {
+	console.log('Example app is started at http://localhost:3001')
+})
