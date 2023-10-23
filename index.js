4bbfcb603806dbf9e7f9a812b489b5126e4e78b5
Elerin Noorkoiv <elerin.noorkoiv@voco.ee>
2023-10-23 15:13:46 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const fs = require('fs');
 
 
 // use ejs files to prepare templates for views
@@ -8,7 +9,16 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req ,res) => {
-	res.render('index')
+	// get data from file
+	fs.readFile('./tasks', 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		// task list data from file
+		const tasks = data.split("\n")
+		res.render('index', {tasks: tasks})
+	});
 })
 
 app.listen(3001, () => {
