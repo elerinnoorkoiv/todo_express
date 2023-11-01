bc582ac94e1542a2eae93d867cc2c2e19575318a
Elerin Noorkoiv <elerin.noorkoiv@voco.ee>
2023-11-01 17:27:32 +0200

    Write data to file

--- index.js
+++ index.js
@@ -2,47 +2,53 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 
-
 // use ejs files to prepare templates for views
 const path = require('path')
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-const readFile = (tasks) => {
+const readFile = (filename) => {
 	return new Promise((resolve, reject) => {
-		// get data from file
-		fs.readFile('./tasks', 'utf8', (err, data) => {
+		//get data from file
+		fs.readFile(filename, 'utf8', (err, data) => {
 			if (err) {
 				console.error(err);
 				return;
 			}
-			// tasks list data from file
+			//tasks list data from file
 			const tasks = data.split("\n")
 			resolve(tasks)
 		});
 	})
 }
 
-app.get('/', (req ,res) => {
-		// task list data from file
-		readFile('./tasks')
-			.then(tasks => {
-				console.log(tasks)
-				res.render('index', {tasks: tasks})
-	 })
-})
-
+app.get('/', (req, res) => {
+	// tasks list data from file
+	readFile('./tasks')
+		.then(tasks => {
+			console.log(tasks)
+			res.render('index', {tasks: tasks})
+		})
+	})
 
 // For parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
-	// tasks list data from file 
+	// tasks list data from file
 	readFile('./tasks')
 		.then(tasks => {
-			//add from sent task to tasks array
+			//add form sent tasks to tasks array
 			tasks.push(req.body.task)
-			console.log(tasks)
+			const data = tasks.join("\n")
+			fs.writeFile('./tasks', data, err => {
+				if(err) {
+					console.error(err);
+					return;
+				}
+				// redirect to / to see result
+				res.redirect('/')
+			})
 		})
 })
 
